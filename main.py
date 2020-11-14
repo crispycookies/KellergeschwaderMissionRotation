cc17f4a6009c8b2415ece430caf0d26d851110a5
tobi <egtobi@gmail.com>
2020-11-14 17:52:50 +0100

    Parameterized Script

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import psutil
 import datetime
@@ -31,7 +32,6 @@ def launch_process(instance, launch_params, launch_prefix, mission, launch_timeo
 
 
 def in_current_time_slice(weekday, test):
-    print(datetime.datetime.today().weekday())
     if datetime.datetime.today().weekday() == weekday:
         return True
     return False
@@ -48,22 +48,31 @@ def block_if_running(instance_to_kill, weekday, refresh_rate):
         print("Process Died")
 
 
-instance = "E:/SteamLibrary/steamapps/common/DCSWorld/bin/DCS.exe"  # Replace with your Path
-launch_timeout = 5  # In Seconds
-launch_prefix = "--net"
-launch_params = "--force_disable_VR"
-instance_to_kill = "DCS.exe"
-refresh_rate = 1  # In Seconds
+def parse_cmd():
+    mission_list = {}
+    params = sys.argv
 
-mission_list = {
-    1: "E:/SteamLibrary/steamapps/common/Mission/Operation_Clear_Field_v123_5.miz",  # Monday
-    2: "E:/SteamLibrary/steamapps/common/Mission/Georgian_Power_v080.miz",  # Tuesday
-    3: "E:/SteamLibrary/steamapps/common/Mission/Persian_Power_v080.miz",  # Wednesday
-    4: "E:/SteamLibrary/steamapps/common/Mission/Syrian_Power_v080.miz",  # Thursday
-    5: "E:/SteamLibrary/steamapps/common/Mission/Operation_Clear_Field_v123_5.miz",  # Friday
-    6: "E:/SteamLibrary/steamapps/common/Mission/Operation_Clear_Field_v123_5.miz",  # Saturday
-    7: "E:/SteamLibrary/steamapps/common/Mission/Operation_Clear_Field_v123_5.miz",  # Sunday
-}
+    if len(params) != 10:
+        print("7 Missions must be provided ", len(params) - 1, " were provided")
+        exit()
+
+    instance = str(params[1])
+    instance_to_kill = str(params[2])
+
+    print("The following Missions will be launched:")
+    for i in range(3, 10):
+        mission_list[i - 3] = params[i]
+        print("Weekday:", i - 3, "Mission:", str(params[i]))
+
+    return mission_list, instance, instance_to_kill
+
+
+launch_timeout = 10  # In Seconds
+launch_prefix = "--net"
+launch_params = ""
+refresh_rate = 60  # In Seconds
+
+mission_list, instance, instance_to_kill = parse_cmd()
 
 while True:
     weekday = datetime.datetime.today().weekday()
